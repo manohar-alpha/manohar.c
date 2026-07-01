0abda5988c3a9b8ae8a1282d000a5e2c32595e89
manohar-alpha <alladasaidmanohar@gmail.com>
2026-07-01 10:43:44 +0530

    Add files via upload

---
+++ manohar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main() {
+  int first, second, temp;
+  first = 10;
+  second =20;
+  // swapping first and second
+  temp = first;
+  first = second;
+  second = temp;
+  printf("After swap: %d,%d",first,second);
+}#include<studio.h>
+void main()
+{
+int a=10;
+printf("%d",&a);
+}
